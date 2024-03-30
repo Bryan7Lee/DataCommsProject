84a9f477e323c9b1c746eb9fb99c243f8bc3e489
Bryan Lee <seojunleeb@gmail.com>
2024-03-30 16:07:34 -0400

    organized all folders

--- client/src/screens/home/home.js
+++ client/src/screens/home/home.js
@@ -1,20 +1,23 @@
 import React from "react";
 import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import io from 'socket.io-client'
-import Library from "./library";
-import Player from "./player";
-import GroupSession from "./groupsession";
+import Library from "../library/library";
+import Player from "../player/player";
+import GroupSession from "../groupsession/groupsession";
+import "./home.css";
 const socket = io.connect("https://localhost:3001")
 
 export default function Home() {
 
   return (
     <Router>
+      <div className="main-body">
         <Routes>
             <Route path="/" element={<Library />} />
             <Route path="/player" element={<Player />} />
             <Route path="/groupsession" element={<GroupSession />} />
         </Routes>
+        </div>
     </Router>
   );
 }
