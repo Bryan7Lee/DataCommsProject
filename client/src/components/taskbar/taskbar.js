b929da31f8ce153011903a4180b9ac66c3d1aa39
Bryan Lee <seojunleeb@gmail.com>
2024-03-30 22:31:01 -0400

    taskBar update

--- client/src/components/taskbar/taskbar.js
+++ client/src/components/taskbar/taskbar.js
@@ -1,9 +1,15 @@
 import React from "react";
 import "./taskbar.css";
+import TaskbarButton from "./taskbarButton";
 
+// bar where you can switch between music player, library, group session
 export default function Taskbar(){
     return <div className="taskbar">
         <img src="https://static.thenounproject.com/png/2942307-200.png" className="profile-img"></img>
-        Your Library
+        <div>
+            <TaskbarButton />
+            <TaskbarButton />
+            <TaskbarButton />
+        </div>
     </div>;
 }
---
+++ client/src/components/taskbar/taskbarButton.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./taskbarButton.css";
+
+// bar where you can switch between music player, library, group session
+export default function TaskbarButton(){
+    return <div className="">
+        B
+    </div>;
+}
