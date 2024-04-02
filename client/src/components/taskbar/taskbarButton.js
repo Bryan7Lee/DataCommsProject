062106fc7046d60bd501791ce795d0c6bffcc678
Bryan Lee <seojunleeb@gmail.com>
2024-04-02 15:39:13 -0400

    finished page swapping

--- client/src/components/taskbar/taskbarButton.js
+++ client/src/components/taskbar/taskbarButton.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "./taskbarButton.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 // bar where you can switch between music player, library, group session
 export default function TaskbarButton(props){
+    const location = useLocation();
+
+    const isActive = location.pathname === props.to;
+  
+    const btnClass = isActive ? "btn-body active" : "btn-body";
+
     return (
         <Link to={props.to}>
-            <div className="btn-body">
+            <div className={btnClass}>
                 {props.icon}
                 <p className="btn-title">{props.title}</p>
             </div>
