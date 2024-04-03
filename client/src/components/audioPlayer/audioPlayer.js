8f7bfd9843da727226370a5cf55e6367267b45d5
Bryan Lee <seojunleeb@gmail.com>
2024-04-02 22:29:42 -0400

    updated GUI

--- client/src/components/audioPlayer/audioPlayer.js
+++ client/src/components/audioPlayer/audioPlayer.js
@@ -1,6 +1,34 @@
 import React from "react";
 import "./audioPlayer.css";
 
-export default function audioPlayer(){
-    return <div className="taskbar"></div>;
+export default function AudioPlayer(){
+    return (
+    <div className="MUSICPLAYERBODYHERE">
+        <div className="img-area">
+            <img src="https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630" alt =""></img>
+        </div>
+        <div className="song-details">
+            <p className="song-title">Song Title</p>
+            <p className="song-artist">Artist</p>
+        </div>
+        <div className="progress-area">
+            <div className="progress-bar">
+                <span></span>
+            </div>
+            <div className="timestamps">
+                <span className="current">0:00</span>
+                <span className="max">3:00</span>
+            </div>
+        </div>
+        <div className="controls">
+            <i id="repeat-plist" class="material-icons" title="Playlist looped">repeat</i>
+            <i id="prev" class="material-icons">skip_previous</i>
+            <div class="play-pause">
+                <i class="material-icons play">play_arrow</i>
+            </div>
+            <i id="next" class="material-icons">skip_next</i>
+            <i id="more-music" class="material-icons">queue_music</i>
+        </div>
+    </div>
+    );
 }
--- client/src/screens/player/player.js
+++ client/src/screens/player/player.js
@@ -1,24 +1,31 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./player.css";
+import AudioPlayer from "../../components/audioPlayer/audioPlayer"
+import Queue from "../../components/queue/queue"
 import { useLocation } from "react-router-dom";
 import { IconContext } from "react-icons";
 import { MdQueueMusic } from "react-icons/md";
-import AudioPlayer from "../../components/audioPlayer/audioPlayer"
 
 export default function Player() {
-
   const location = useLocation();
   console.log(location);
+  const [tracks, setTracks] = useState([]);
+  const [currentTrack, setCurrentTrack] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   return (
-    <div className = "body flex">
-      <div className = "player-body"></div>
+    <div className = "page-body flex">
+
+      <div className = "player-body">
+        <AudioPlayer/>
+      </div>
+
       <div className = "queue-body">
-      <div className="btn-body">
-        <IconContext.Provider value={{ size: "22px", className: "btn-icon" }}>
-          {<MdQueueMusic />}
-          <p className="lib-title">{"Queue"}</p>
-        </IconContext.Provider>
+        <div className="btn-body">
+          <IconContext.Provider value={{ size: "22px", className: "btn-icon" }}>
+            {<MdQueueMusic />}
+            <p className="lib-title">{"Queue"}</p>
+          </IconContext.Provider>
         </div>
       </div>
     </div>
